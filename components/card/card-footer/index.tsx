af19bd4b20d887130599a7140c36fb02d6656a51
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-15 11:03:53 +0530

    updated links padding

--- components/card/card-footer/index.tsx
+++ components/card/card-footer/index.tsx
@@ -5,13 +5,13 @@ const CarCardFooter = () => {
   return (
     <div className="flex flex-col justify-between p-4 lg:flex-row lg:space-x-2">
       <Link href={"/"}>
-        <a className="px-8 py-2 mt-2 tracking-wide rounded-lg font-bold text-center text-white bg-primary hover:bg-primary_hover">
+        <a className="px-4 py-2 mt-2 tracking-wide rounded-lg font-bold text-center text-white bg-primary hover:bg-primary_hover">
           Enquire Now
         </a>
       </Link>
 
       <Link href={"/"}>
-        <a className="px-8 py-2 mt-2 tracking-wide rounded-lg  font-bold text-center text-primary opacity-80 border border-primary hover:bg-primary_hover hover:text-white">
+        <a className="px-4 py-2 mt-2 tracking-wide rounded-lg  font-bold text-center text-primary opacity-80 border border-primary hover:bg-primary_hover hover:text-white">
           View Details
         </a>
       </Link>
