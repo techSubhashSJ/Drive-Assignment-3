f7de8011813c4a5993b0e704b274c66ccdbe48b1
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-15 09:34:22 +0530

    load more feature added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,72 @@
 import type { NextPage } from "next";
-import { gql } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import client from "../apollo-client";
 import Layout from "../components/layout";
+import AlertMessage from "../components/alert-message";
 import CarCard from "../components/card";
-import { DealerListingsType } from "../types";
+import { DealerListingsType, pageInfoType } from "../types";
+import Button from "../components/button";
+import { useState } from "react";
+import { GET_CARS, LOAD_MORE_CARS } from "../graphql-queries";
+import Spinner from "../components/spinner";
 
-type HomePagePropsType = { cars: DealerListingsType[] };
+type HomePagePropsType = {
+  cars: DealerListingsType[];
+  pageInfo: pageInfoType;
+};
 
-const Home: NextPage<HomePagePropsType> = ({ cars }) => {
+const Home: NextPage<HomePagePropsType> = ({ cars, pageInfo }) => {
+  const [carsData, setCarsData] = useState<DealerListingsType[]>(cars);
+  const [pageInformation, setPageInformation] = useState(pageInfo);
+  const [error, setError] = useState<string | null>(null);
+
+  const [fetchPosts, { loading }] = useLazyQuery(LOAD_MORE_CARS, {
+    notifyOnNetworkStatusChange: true,
+    onCompleted: (data) => {
+      /**
+       * Call setCarsData to concat the new set of cars to existing one and update pageInfo
+       * that contains the information about whether we have the next page.
+       */
+      setCarsData((cars) => cars.concat(data.DealerListings.results));
+      setPageInformation(data.DealerListings.pageInfo);
+    },
+    onError: (error) => {
+      setError("Something went wrong");
+    },
+  });
+
+  const handleSubmit = () => {
+    fetchPosts({
+      variables: {
+        page: pageInformation.currentPage + 1,
+      },
+    });
+  };
+
   return (
     <Layout>
+      <> {error && <AlertMessage error={error} />} </>
+
       <div className="xl:flex xl:justify-center">
-        <div className="mx-[5%] my-[2%] gap-4 grid md:grid-cols-2 lg:grid-cols-3 max-w-screen-2xl">
-          {cars.slice(0, 9).map((car, index) => (
+        <div className="mx-[5%] my-[2%] gap-4 grid md:grid-cols-2 xl:grid-cols-3 max-w-screen-xl ">
+          {carsData.map((car, index) => (
             <CarCard car={car} key={index} />
           ))}
         </div>
+      </div>
+
+      <div className="flex justify-center">
+        {loading ? (
+          <Spinner />
+        ) : (
+          pageInformation.hasNextPage && (
+            <Button
+              label="Load More"
+              classes="px-4 py-2 my-4 rounded-lg bg-primary text-white hover:bg-primary_hover"
+              onclick={handleSubmit}
+            />
+          )
+        )}
       </div>
     </Layout>
   );
@@ -25,38 +76,13 @@ export default Home;
 
 export async function getStaticProps() {
   const { data } = await client.query({
-    query: gql`
-      {
-        DealerListings {
-          results {
-            id
-            colour
-            vehicleKey: vehicle_key
-            listType: list_type
-            year
-            price: price_excluding_government_charges
-            kms: odometer
-            variant
-            fuelType: fuel_type
-            transmission
-            engineSize: engine_size
-            seat_capacity
-            image: main_image
-            state: location_state
-            makeName: make
-            modelName: model
-            region: Region {
-              regionName: region_name
-            }
-          }
-        }
-      }
-    `,
+    query: GET_CARS,
   });
 
   return {
     props: {
       cars: data.DealerListings.results,
+      pageInfo: data.DealerListings.pageInfo,
     },
   };
 }
