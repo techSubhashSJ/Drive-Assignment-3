ef10dee4eab5c32669f72d513fb21bfc582d4138
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-15 10:46:49 +0530

    update car image style

--- components/card/card-image/index.tsx
+++ components/card/card-image/index.tsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const CarCardImage = (props: { src: string }) => {
   return (
-    <img
-      alt="Image not available"
-      className="block h-72 w-full object-cover"
-      src={props.src}
-      data-testid="image"
-    />
+    <Link href={"/"}>
+      <a>
+        <img
+          alt="Image not available"
+          className="block h-72 w-full object-cover transition ease-in-out delay-150 duration-300 hover:scale-110"
+          src={props.src}
+          data-testid="image"
+        />
+      </a>
+    </Link>
   );
 };
 
