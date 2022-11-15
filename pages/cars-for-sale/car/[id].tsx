da51a7c093ea4c47cb2af4c6b1ad584f4fe582aa
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-15 11:27:15 +0530

    Created dynamic route page for a single car

---
+++ pages/cars-for-sale/car/[id].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../../components/layout";
+
+const Car = () => {
+  return (
+    <Layout>
+      <div>Hello</div>
+    </Layout>
+  );
+};
+
+export default Car;
