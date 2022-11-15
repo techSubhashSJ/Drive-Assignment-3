3eee2f2e3ed114d20973c8f2b4149335d80b3d96
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-15 09:36:42 +0530

    graphql queries added

---
+++ graphql-queries/index.tsx
@@ -0,0 +1,69 @@
+import { gql } from "@apollo/client";
+
+export const GET_CARS = gql`
+  {
+    DealerListings(paginate: { page: 1, pageSize: 9 }) {
+      results {
+        id
+        colour
+        vehicleKey: vehicle_key
+        listType: list_type
+        year
+        price: price_excluding_government_charges
+        kms: odometer
+        variant
+        fuelType: fuel_type
+        transmission
+        engineSize: engine_size
+        seat_capacity
+        image: main_image
+        state: location_state
+        makeName: make
+        modelName: model
+        region: Region {
+          regionName: region_name
+        }
+      }
+      pageInfo {
+        hasNextPage
+        currentPage
+        pageCount
+        itemCount
+      }
+    }
+  }
+`;
+
+export const LOAD_MORE_CARS = gql`
+  query LOAD_MORE($page: Int) {
+    DealerListings(paginate: { page: $page, pageSize: 9 }) {
+      results {
+        id
+        colour
+        vehicleKey: vehicle_key
+        listType: list_type
+        year
+        price: price_excluding_government_charges
+        kms: odometer
+        variant
+        fuelType: fuel_type
+        transmission
+        engineSize: engine_size
+        seat_capacity
+        image: main_image
+        state: location_state
+        makeName: make
+        modelName: model
+        region: Region {
+          regionName: region_name
+        }
+      }
+      pageInfo {
+        hasNextPage
+        currentPage
+        pageCount
+        itemCount
+      }
+    }
+  }
+`;
