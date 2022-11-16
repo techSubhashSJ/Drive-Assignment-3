e836365d3cc7aab20c0f9c361a7fc2de53675008
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-16 14:11:52 +0530

    Increased card height

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -4,13 +4,17 @@ import CarCardImage from "./card-image";
 import { DealerListingsType } from "../../types";
 import CarCardFooter from "./card-footer";
 
-const CarCard = (props: { car: DealerListingsType }) => {  
+const CarCard = (props: { car: DealerListingsType }) => {
   return (
     <div>
-      <article className="overflow-hidden rounded-lg shadow-lg">
-        <CarCardImage src={props?.car?.image} id={props.car.id}/>
-        <CarCardBody car={props.car} />
-        <CarCardFooter id={props.car.id}/>
+      <article className="overflow-hidden rounded-lg shadow-lg h-full">
+        <div className="flex flex-col justify-between h-full">
+          <div>
+            <CarCardImage src={props?.car?.image} id={props.car.id} />
+            <CarCardBody car={props.car} />
+          </div>
+          <CarCardFooter id={props.car.id} />
+        </div>
       </article>
     </div>
   );
