fd06a16ed2614c9ffd3cd654bbc0d025da5d2f5c
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-16 13:58:37 +0530

    moved graphql api url into env file

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://dev-carsforsale.drivemustang.com.au/graphql",
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_API,
     cache: new InMemoryCache(),
 });
 
